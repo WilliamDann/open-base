613087ce8a6cd50722bea8bcac012cfcb2683b38
WilliamDann <s-william.dann@lwtech.edu>
2024-03-03 22:21:56 -0800

    basic moving pieces

--- public/module/board/board.js
+++ public/module/board/board.js
@@ -1,14 +1,64 @@
 import { Chess } from '../../../node_modules/chess.js/dist/esm/chess.js'
 
+/**
+ * Called when a square has a mouse down event
+ * @param {Event} e 
+ */
+function onSquareDown(e)
+{
+    e.preventDefault();
+    window.click = e.target.parentElement.id;
+}
+
+/**
+ * Called when a square has a mouse up event
+ * @param {Event} e 
+ */
+function onSquareUp(e)
+{
+    e.preventDefault();
+
+    let to;
+    if (!e.target.id)
+        to = e.target.parentElement.id;
+    else
+        to = e.target.id;
+
+    let move = `${window.click}${to}`;
+    
+    const event = new CustomEvent('move', { detail: move });
+    window.dispatchEvent(event);
+}
+
 export default class Board
 {
-    constructor(x=8, y=8)
+    /**
+     * Class to control a board
+     * @param {HTMLElement} board The board container element
+     * @param {number} x x size of the board (default 8)
+     * @param {number} y y size of the board (default 8)
+     */
+    constructor(board, x=8, y=8)
     {
+        this.board = board;
         this.x = x;
         this.y = y;
     }
 
-    drawPieces(fen)
+    /**
+     * Clear the board container
+     * @param {HTMLElement} elem board container
+     */
+    clear()
+    {
+        this.board.innerHTML = "";
+    }
+
+    /**
+     * Sets the board to a given position using a fen string
+     * @param {string} fen FEN string of the desired position
+     */
+    setFEN(fen)
     {
         let chess = new Chess(fen);
         for (let piece of chess.board().flat().filter(x => x!= null))
@@ -19,7 +69,7 @@ export default class Board
             elem.width = "64";
             elem.height = "64";
 
-            document.querySelector(`#square_${piece.square}`).appendChild(elem);
+            document.querySelector(`#${piece.square}`).appendChild(elem);
         }
     }
 
@@ -34,30 +84,44 @@ export default class Board
         let file = document.createElement('div');
         file.className = 'file';
 
-        for (let i = 0; i < this.y; i++)
+        for (let i = this.y; i > 0; i--)
         {
-            let square = document.createElement('div');
-            square.id  = `square_${fileName}${i+1}`;
-
-            square.className = 'square ' + (white ? 'light' : 'dark');
+            file.appendChild(this.drawSquare(`${fileName}${i}`, white));
             white = !white;
-            file.appendChild(square);
         }
 
         return file;
     }
 
     /**
+     * Create an html element for a square
+     * @param {string} name Name of the square
+     * @param {boolean} white if the square is white
+     * @returns {HTMLElement}
+     */
+    drawSquare(name, white)
+    {
+        let square = document.createElement('div');
+        square.id  = name;
+
+        square.addEventListener('mousedown', onSquareDown);
+        square.addEventListener('mouseup', onSquareUp);
+
+        square.className = 'square ' + (white ? 'light' : 'dark');
+        return square
+    }
+
+    /**
      * Draws an HTML board inside a container element
      * @param {HTMLElement} elem HTML element container for the board
      */
-    draw(elem)
+    draw()
     {
         let white = true;
 
         for (let i = 0; i < this.x; i++)
         {
-            elem.appendChild(this.drawFile(String.fromCharCode(97+i), white));
+            this.board.appendChild(this.drawFile(String.fromCharCode(97+i), white));
             white = !white;
         }
     }
